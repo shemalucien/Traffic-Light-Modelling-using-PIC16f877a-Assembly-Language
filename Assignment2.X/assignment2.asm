68eb9f6362e6144829c3a9a397c63b7edef6d8d6
shema lucien <shemalucien5@gmail.com>
2021-08-01 20:16:28 +0200

    submission

--- Assignment2.X/assignment2.asm
+++ Assignment2.X/assignment2.asm
@@ -1,60 +1,55 @@
-STATUS equ 03h
-RP0 equ 05
-RP1 equ 06
-PORTA equ 05h
-TRISA equ 85h
- 
- 
- COUNTER equ 20h 
- 
- 
-    org 00 
-    clrw 
-    
-    BCF STATUS, RP0 
-    BCF STATUS, RP1 
+#include "p16f877a.inc"
 
-    CLRF PORTA 
-    
-    BSF STATUS, RP0
-    
-    MOVLW 0x00 
-    
-    MOVWF TRISA 
-    
-    BCF STATUS,	RP0 
-    BCF STATUS,	RP1 
- 
-START	MOVLW  B'00000010'
-	MOVWF PORTA  
-    
-	MOVLW 0xff
-	MOVWF COUNTER 
+; CONFIG
+; __config 0xFFFF
+ __CONFIG _FOSC_EXTRC & _WDTE_ON & _PWRTE_OFF & _BOREN_ON & _LVP_ON & _CPD_OFF & _WRT_OFF & _CP_OFF
+STATUS  equ 03h
+PORTA   equ 05h
+TRISA   equ 85h
+   RP0 equ 05h
+   RP1 equ 06h
 
-LOOP1	DECFSZ COUNTER
-	GOTO LOOP1
-	
-	CLRF PORTA
+COUNTER1 equ 0x0C
+COUNTER2 equ 0x0D
+        org 00h
+        clrw
+        BCF    STATUS,RP0
+        BCF    STATUS,RP1
+        CLRF   PORTA
+        BSF    STATUS, RP0
+        MOVLW  0x00 
+        MOVWF  TRISA
+        BCF    STATUS, RP0
 
-	MOVLW B'00000100'
-	MOVWF PORTA  
-    
-	MOVLW 0xff
-	MOVWF COUNTER 
-	
-LOOP2	DECFSZ COUNTER
-	GOTO LOOP2
-	
-	CLRF PORTA
-	MOVLW B'00001000'
-	MOVWF PORTA
-	
-	MOVLW 0xff
-	MOVWF COUNTER	
-	
-LOOP3	DECFSZ COUNTER
-        GOTO LOOP3
-	
-	GOTO START 
-END 
-	
+START    MOVLW   0x02
+         MOVWF   PORTA
+	 call    first_delay
+	 MOVLW   0x04
+         MOVWF   PORTA
+	 call    first_delay
+	 MOVLW   0x08
+         MOVWF   PORTA
+	 call    second_delay
+
+         GOTO    START
+;delay  
+first_delay MOVLW d'40'
+	   MOVWF COUNTER1
+dec_2      MOVLW d'255'
+	   MOVWF COUNTER2
+dec_1      DECFSZ  COUNTER2,1
+	   goto  dec_1
+	   DECFSZ  COUNTER1,1
+	   goto  dec_2
+	   return
+second_delay MOVLW d'10'
+	   MOVWF COUNTER1
+dec_4      MOVLW d'255'
+	   MOVWF COUNTER2
+dec_3      DECFSZ  COUNTER2,1
+	   goto  dec_3
+	   DECFSZ  COUNTER1,1
+	   goto  dec_4
+	   return
+         
+END
--- Assignment2.X/build/default/production/assignment2.o.d
+++ Assignment2.X/build/default/production/assignment2.o.d
@@ -1,3 +1,3 @@
 build/default/production/assignment2.o:  \
-	C:/Users/ShemaLucien/MPLABXProjects/Assignment2.X/assignment2.asm  \
+	D:/Courses/CSE/4th_Year/Semester\ I/Embedded\ Computer\ Systems\ Engineering/assignment2/Assignment2.X/assignment2.asm  \
 
